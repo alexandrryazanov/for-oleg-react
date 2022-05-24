03e2c0f860869080dabce3774520622b00094c7b
mixan <adon1999@mail.ru>
2022-05-24 16:22:53 +0300

    creating pagination

--- src/Michael/pages/Users/index.tsx
+++ src/Michael/pages/Users/index.tsx
@@ -5,18 +5,37 @@ import { UserData } from "./UsersTable/types";
 const UsersTab = () => {
   const [users, setUsers] = useState<UserData[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [offset, setOffset] = useState(0);
+  const [limit, setLimit] = useState(2);
 
   useEffect(() => {
     setIsLoading(true);
-    fetch("https://jsonplaceholder.typicode.com/users")
+    fetch(
+      `https://jsonplaceholder.typicode.com/users?_limit=${limit}&_start=${offset}`
+    )
       .then((response) => response.json())
       .then((json) => setUsers(json))
       .finally(() => setIsLoading(false));
-  }, []);
+  }, [offset, limit]);
 
   return (
-    <div style={{ height: "calc(100vh - 150px)", width: "100%" }}>
-      {isLoading ? "Loading..." : <UsersTable data={users} />}
+    <div
+      style={{
+        height: "calc(100vh - 230px)",
+        width: "100%",
+      }}
+    >
+      {isLoading ? (
+        "Loading..."
+      ) : (
+        <UsersTable
+          data={users}
+          setOffset={setOffset}
+          offset={offset}
+          limit={limit}
+          setLimit={setLimit}
+        />
+      )}
     </div>
   );
 };
